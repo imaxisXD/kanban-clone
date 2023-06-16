688895fc5cc2be4e6a2f0e25da3617d4b1d4932e
imaxisXD <sunny735084@gmail.com>
2023-06-16 16:41:08 +0530

    wip: nav bar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -5,9 +5,9 @@ export default function NavBar() {
     return (
         <nav className="bg-white text-black flex h-20 items-center sticky inset-x-0 top-0 z-30 w-full">
             <div className="flex items-center justify-between pr-10">
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center mr-11">
                     <Image src='navbar-logo.svg' alt="project-logo" width={24} height={24} />
-                    <h1>Project M.</h1>
+                    <h1>Project M. ss</h1>
                 </div>
                 <Image src='left-arrow.svg' alt="left-arrow" width={26} height={20} />
             </div>
